2bf056b531fe8156e960ddff78d7260f7df20020
rtritto <ariele.1994@gmail.com>
2024-09-06 07:43:52 +0000

    Add @elysiajs/cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,11 @@
 import { Elysia } from 'elysia'
 import { vike } from 'vike-node/elysia'
+import { cors } from '@elysiajs/cors'
 
 const port = +(process.env.PORT || 3000)
 
-new Elysia()
+const app = new Elysia()
+app
+  .use(cors())
   .use(vike())
   .listen(port, () => console.log(`Server running at http://localhost:${port}`))
