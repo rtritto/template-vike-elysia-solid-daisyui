cc19694715724d5c1775a04894ef7c8b6b6f9d21
rtritto <ariele.1994@gmail.com>
2024-09-11 10:22:47 +0200

    Use Bun server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
 import { Elysia } from 'elysia'
 import { vike } from 'vike-node/elysia'
+import { serve } from 'bun'
 
 const port = +(process.env.PORT || 3000)
 
 const app = new Elysia()
 app.use(vike())
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+// app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+serve({
+  fetch: app.fetch,
+  port
+})
