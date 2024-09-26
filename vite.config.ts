9dc99448259b054aff2ef2ec1d0e572104e704b8
rtritto <ariele.1994@gmail.com>
2024-09-27 00:32:35 +0200

    Add missing vike plugin and disable vike-node plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+import vike from 'vike/plugin'
 import vikeSolid from 'vike-solid/vite'
-import { vikeNode } from 'vike-node/plugin'
+// import { vikeNode } from 'vike-node/plugin'
 import type { UserConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -8,8 +9,9 @@ import { resolve } from 'node:path'
 export default {
   cacheDir: '.vite',
   plugins: [
-    vikeNode('./src/server/index.ts'),
+    vike(),
     vikeSolid()
+    // vikeNode('./src/server/index.ts')
   ],
   server: {
     port: 3000
